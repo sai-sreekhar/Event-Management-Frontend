d3c1b11f271dba33f66177bb1e48d864b3a8d988
sai-sreekhar <sai.sreekhar@gmail.com>
2023-11-06 17:48:55 +0530

    feat: added page not found component

--- Event-Management-Frontend/src/App.jsx
+++ Event-Management-Frontend/src/App.jsx
@@ -4,13 +4,17 @@ import Login from "./components/Login";
 import store from "./redux/store";
 import { Route, Routes } from "react-router";
 import SignUp from "./components/Signup";
+import NoMatch from "./components/NoMatch";
+import Home from "./components/Home";
 
 function App() {
   return (
     <Provider store={store}>
       <Routes>
+        <Route path="/" element={<Home></Home>} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
+        <Route path="*" element={<NoMatch></NoMatch>}></Route>
       </Routes>
     </Provider>
   );
