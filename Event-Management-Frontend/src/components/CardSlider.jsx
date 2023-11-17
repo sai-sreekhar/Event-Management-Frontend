35fd63ec2398f23a5e40e10dff0210c9b3ac91fb
sai-sreekhar <sai.sreekhar@gmail.com>
2023-11-17 14:58:50 +0530

    fix: fixed import error

--- Event-Management-Frontend/src/components/CardSlider.jsx
+++ Event-Management-Frontend/src/components/CardSlider.jsx
@@ -2,7 +2,7 @@ import React, { useState, useRef } from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-import './CardSlider.css';
+import './../styles/CardSlider.css';
 import { FaCalendarDays, FaLocationDot } from "react-icons/fa6";
 
 const CardSlider = () => {
