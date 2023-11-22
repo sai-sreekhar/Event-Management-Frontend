3f899c4357d0fb401cd54836c8ec78afaf60c16d
sai-sreekhar <sai.sreekhar@gmail.com>
2023-11-22 12:27:54 +0530

    feat: styles changed

--- Event-Management-Frontend/src/components/MyEvents.jsx
+++ Event-Management-Frontend/src/components/MyEvents.jsx
@@ -90,135 +90,141 @@ const MyEvents = () => {
 
   return (
     <>
-    
-      <Backdrop
-        sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
-        open={backdropOpen}
-      >
-        <CircularProgress color="inherit" />
-      </Backdrop>
+      <div style={{ height: "130vh" }}>
+        <Backdrop
+          sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
+          open={backdropOpen}
+        >
+          <CircularProgress color="inherit" />
+        </Backdrop>
 
-      <Snackbar
-        open={snackbarOpen}
-        autoHideDuration={6000}
-        onClose={handleClose}
-      >
-        <Alert onClose={handleClose} severity="error" sx={{ width: "100%" }}>
-          {events.errorReason}
-        </Alert>
-      </Snackbar>
-      <Box
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-          height: "100%",
-          width: "100%",
-          marginTop: "-250px",
-        }}
-      >
-        <Typography variant="h3" align="center" marginBottom="30px" sx={{color:'white'}}>
-          MY HOSTED EVENTS
-        </Typography>
+        <Snackbar
+          open={snackbarOpen}
+          autoHideDuration={6000}
+          onClose={handleClose}
+        >
+          <Alert onClose={handleClose} severity="error" sx={{ width: "100%" }}>
+            {events.errorReason}
+          </Alert>
+        </Snackbar>
+        <Box
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "100%",
+            width: "100%",
+            marginTop: "-250px",
+          }}
+        >
+          <Typography
+            variant="h3"
+            align="center"
+            marginBottom="30px"
+            sx={{ color: "white" }}
+          >
+            MY HOSTED EVENTS
+          </Typography>
 
-        <Grid container spacing={2} pl={2} pr={2}>
-          {events.myHostedEvents.map((event) => (
-            <Grid item xs={12} sm={6} md={4} key={event._id}>
-              <Card>
-                <CardActionArea
-                  onClick={() => {
-                    navigate(`/browseEvents/${event._id}`);
-                  }}
-                >
-                  <CardMedia
-                    component="img"
-                    height="140"
-                    image={event.image}
-                    alt="Event Image"
-                  />
-                  <CardContent>
-                    <Typography gutterBottom variant="h5" component="div">
-                      {event.name}
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                      {event.location}
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                      {new Date(Number(event.date)).toUTCString()}
-                    </Typography>
-                  </CardContent>
-                </CardActionArea>
-                <CardActions>
-                  <Button
-                    size="small"
-                    sx={{
-                      backgroundColor: "blue",
-                      ":hover": { backgroundColor: "green" },
-                    }}
+          <Grid container spacing={2} pl={2} pr={2}>
+            {events.myHostedEvents.map((event) => (
+              <Grid item xs={12} sm={6} md={4} key={event._id}>
+                <Card>
+                  <CardActionArea
                     onClick={() => {
                       navigate(`/browseEvents/${event._id}`);
                     }}
                   >
-                    <Typography variant="body2" color="white">
-                      View Event
-                    </Typography>
-                  </Button>
-                  <Button
-                    size="small"
-                    sx={{
-                      backgroundColor: "blue",
-                      ":hover": { backgroundColor: "green" },
-                    }}
-                    onClick={() => {
-                      navigate(`/updateEvent/${event._id}`);
-                    }}
-                  >
-                    <Typography variant="body2" color="white">
-                      Update Details
-                    </Typography>
-                  </Button>
-                  <Button
-                    size="small"
-                    sx={{
-                      backgroundColor: "blue",
-                      ":hover": { backgroundColor: "green" },
-                    }}
-                    onClick={() => {
-                      navigate(`/myEvents/registrations/${event._id}`);
-                    }}
-                  >
-                    <Typography variant="body2" color="white">
-                      View Registrations
-                    </Typography>
-                  </Button>
-                  <LoadingButton
-                    size="small"
-                    sx={{
-                      backgroundColor: "red",
-                      ":hover": { backgroundColor: "darkred" },
-                    }}
-                    onClick={() => {
-                      deleteEventHandler(event._id);
-                    }}
-                    variant="contained"
-                    loading={
-                      events.apiStatus === apiStatus.IN_PROGRESS &&
-                      events.eventOperation ===
-                        eventOperations.DELETE_MY_HOSTED_EVENT &&
-                      eventidToDelete === event._id
-                    }
-                  >
-                    <Typography variant="body2" color="white">
-                      Delete Event
-                    </Typography>
-                  </LoadingButton>
-                </CardActions>
-              </Card>
-            </Grid>
-          ))}
-        </Grid>
-      </Box>
+                    <CardMedia
+                      component="img"
+                      height="140"
+                      image={event.image}
+                      alt="Event Image"
+                    />
+                    <CardContent>
+                      <Typography gutterBottom variant="h5" component="div">
+                        {event.name}
+                      </Typography>
+                      <Typography variant="body2" color="text.secondary">
+                        {event.location}
+                      </Typography>
+                      <Typography variant="body2" color="text.secondary">
+                        {new Date(Number(event.date)).toUTCString()}
+                      </Typography>
+                    </CardContent>
+                  </CardActionArea>
+                  <CardActions>
+                    <Button
+                      size="small"
+                      sx={{
+                        backgroundColor: "blue",
+                        ":hover": { backgroundColor: "green" },
+                      }}
+                      onClick={() => {
+                        navigate(`/browseEvents/${event._id}`);
+                      }}
+                    >
+                      <Typography variant="body2" color="white">
+                        View Event
+                      </Typography>
+                    </Button>
+                    <Button
+                      size="small"
+                      sx={{
+                        backgroundColor: "blue",
+                        ":hover": { backgroundColor: "green" },
+                      }}
+                      onClick={() => {
+                        navigate(`/updateEvent/${event._id}`);
+                      }}
+                    >
+                      <Typography variant="body2" color="white">
+                        Update Details
+                      </Typography>
+                    </Button>
+                    <Button
+                      size="small"
+                      sx={{
+                        backgroundColor: "blue",
+                        ":hover": { backgroundColor: "green" },
+                      }}
+                      onClick={() => {
+                        navigate(`/myEvents/registrations/${event._id}`);
+                      }}
+                    >
+                      <Typography variant="body2" color="white">
+                        View Registrations
+                      </Typography>
+                    </Button>
+                    <LoadingButton
+                      size="small"
+                      sx={{
+                        backgroundColor: "red",
+                        ":hover": { backgroundColor: "darkred" },
+                      }}
+                      onClick={() => {
+                        deleteEventHandler(event._id);
+                      }}
+                      variant="contained"
+                      loading={
+                        events.apiStatus === apiStatus.IN_PROGRESS &&
+                        events.eventOperation ===
+                          eventOperations.DELETE_MY_HOSTED_EVENT &&
+                        eventidToDelete === event._id
+                      }
+                    >
+                      <Typography variant="body2" color="white">
+                        Delete Event
+                      </Typography>
+                    </LoadingButton>
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Box>
+      </div>
     </>
   );
 };
--- Event-Management-Frontend/src/components/Profile.jsx
+++ Event-Management-Frontend/src/components/Profile.jsx
@@ -4,7 +4,7 @@ import withAuth from "./withAuth";
 const Profile = () => {
   const auth = useSelector((state) => state.auth);
   return (
-    <div>
+    <div style={{height:'130vh'}}>
       <h1 style={{textAlign:'center',color:'white',paddingTop:'50px'}}>Profile Page</h1>
       <h2 style={{textAlign:'center',color:'white'}}>Welcome {auth.userData.name}</h2>
     </div>
